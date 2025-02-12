9e6e8f14cecb8ac0ff6a24a4d1f8cac953e2379c
MiguelA-Zurita <mzurita@nigul.cide.es>
2025-02-12 23:19:49 +0100

    Finished exercise Hamburguer menu

--- JS/Activitat3.5/JS/HamburguerScript.js
+++ JS/Activitat3.5/JS/HamburguerScript.js
@@ -3,3 +3,10 @@ hamburguesa.addEventListener("click", function(isOn){
     const menuNav = document.querySelector("nav");
     menuNav.classList.toggle("modoHamburguesa");
 });
+const links = document.querySelectorAll("nav a");
+links.forEach(function(link){
+    link.addEventListener("click", function(){
+        const menuNav = document.querySelector("nav");
+        menuNav.classList.toggle("modoHamburguesa");
+    });
+});
