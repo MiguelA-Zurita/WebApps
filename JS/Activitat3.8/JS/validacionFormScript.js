8ec0ac403df0f08223165d42357a5dd392ef4962
MiguelA-Zurita <mzurita@nigul.cide.es>
2025-02-24 15:30:21 +0100

    Optimized performance

--- JS/Activitat3.8/JS/validacionFormScript.js
+++ JS/Activitat3.8/JS/validacionFormScript.js
@@ -11,15 +11,19 @@ formulario.addEventListener('submit', function(event) {
     if (nombre.value.trim() === '') {
         alert('El campo nombre no puede estar vacío');
         nombre.focus();
+        return false;
     } else if (regExpEmail.test(email.value) == false) {
         alert('El campo email no cumple con el formato de email');
         email.focus();
+        return false;
     } else if (asunto.value.trim() === '') {
         alert('El campo asunto no puede estar vacío');
         asunto.focus();
+        return false;
     } else if (mensaje.value.trim() === '') {
         alert('El campo mensaje no puede estar vacío');
         mensaje.focus();
+        return false;
     } else {
         formulario.submit();
         alert('Formulario enviado correctamente');
