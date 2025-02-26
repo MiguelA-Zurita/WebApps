fc5f2ffe35781d69c5d0bbb9a996b6aec1a8444a
MiguelA-Zurita <mzurita@nigul.cide.es>
2025-02-26 17:11:18 +0100

    new Exercise for to-Do List

---
+++ JS/Activitat3.6/JS/Activitat3.6.js
@@ -0,0 +1,63 @@
+const campoTexto = document.querySelector("input");
+const taskList = document.getElementById("taskList");
+const strawberry = document.getElementById("strawberry");
+
+function addTask(){
+    let newTaskText = campoTexto.value;
+    let newTask = document.createElement("li");
+    let buttonContainer = document.createElement("div");
+    newTask.className = 'task';
+    let newCompletedButton = document.createElement("button");
+    let newDeleteButton = document.createElement("button");
+    newCompletedButton.innerText = 'Task completed';
+    newCompletedButton.className = 'completeButton';
+    newCompletedButton.addEventListener('click', function(event){
+        let boton = event.target;
+        let contenedor = boton.parentNode;
+        let elemento = contenedor.parentNode;
+        elemento.className = 'completedTask';
+        boton.style.display = 'none';
+    })
+    newDeleteButton.innerText = 'Delete Task';
+    newDeleteButton.className = 'deleteButton';
+    newDeleteButton.addEventListener('click', function(event){
+        let boton = event.target;
+        let contenedor = boton.parentNode;
+        let elemento = contenedor.parentNode;
+        elemento.remove();
+    })
+    let newTaskParagraph = document.createElement("p");
+    newTaskParagraph.innerText = newTaskText;
+    buttonContainer.appendChild(newCompletedButton);
+    buttonContainer.appendChild(newDeleteButton);
+    newTask.appendChild(newTaskParagraph);
+    newTask.appendChild(buttonContainer);
+    taskList.appendChild(newTask);
+}
+
+function deleteAllCompletedTasks(){
+    let allCompletedTasks = document.getElementsByClassName('completedTask');
+    for(i = allCompletedTasks.length-1; i>=0; i--){
+        let completedTask = allCompletedTasks[i];
+        completedTask.remove();
+    }
+}
+
+strawberry.addEventListener('click',function(event){
+    let filterList = document.getElementById("filterList");
+    filterList.classList.toggle("inactive");
+});
+
+function filter(event){
+    let eleccionFiltrado = event.target;
+    switch(eleccionFiltrado.value){
+        case ("all"):
+            let allTasks = taskList.childNodes;
+            for(i = allTasks-1; i>=0; i--){
+                let task = allTasks[i];
+                task.style.display = flex;
+            }
+        case("active"):
+        case("completed"):
+    }
+}
