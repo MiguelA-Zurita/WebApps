b82d0574239ca14531514ceeab57fd836a534d0e
MiguelA-Zurita <mzurita@nigul.cide.es>
2025-05-13 15:48:58 +0200

    Finished exercise

--- JS/Activitat3.11/js/Script.js
+++ JS/Activitat3.11/js/Script.js
@@ -12,11 +12,26 @@ formulario.addEventListener("submit", function(event){
     if(origen.value == destino.value ){
         alert("El origen no puede ser el mismo que el destino!");
     }
+
     else if(fechaIda.value < new Date()){
         alert("La fecha de ida no puede ser menor a la fecha actual! No somos una máquina del tiempo");
     }
-    else if(){
-        
-    }
 
+    else if(fechaVuelta.value < fechaIda.value){
+        alert("La fecha de ida no puede ser menor a la fecha de ida! No somos una máquina del tiempo");
+    }
+    
 });
+/* 
+- Para preparar los datos para enviar por get:
+     // Redirigir a la pàgina de confirmació amb els paràmetres de la reserva amb mètode GET
+     var urlCompleta = "dispo.html?paramOrigen=" + encodeURIComponent(origen);
+     window.location.href = urlCompleta;
+
+- Para recoger los datos desde el get:
+     const params = new URLSearchParams(window.location.search);
+     const origen = params.get("paramOrigen");
+
+- Tras recoger los datos con localStorage, si ya no se necesitan, hay que borrarlos con: localStorage.removeItem("paramOrigen"); 
+
+- Si usamos un <select> con <options> y queremos indicar un value diferente al textContent, recogemos el textContent con: querySelector(opcion:checked) y luego el textContent. */
